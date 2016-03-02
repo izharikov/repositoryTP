c2a3efee0b4da55122449d47748b2adb576dae75
izharikov <zharikovigor97@gmail.com>
2016-03-03 00:18:46 +0300

    2nd change

--- 1.c
+++ 1.c
@@ -5,5 +5,6 @@ int main(){
 	scanf("%d", &n);				//read n
 	printf("Your age in days: %d\n", n * 365);	//print age in days
 	printf("For git change#0\n");
+	printf("For git change#1\n");
 	return 0;
 }
